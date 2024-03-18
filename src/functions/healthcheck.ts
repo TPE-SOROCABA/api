07cf34419c318c0eb5f8fd5fa3e495dc07cb3c05
Wilson Felipe <wfelipe2011@gmail.com>
2024-03-18 16:55:17 -0300

    Update healthcheck.ts

--- src/functions/healthcheck.ts
+++ src/functions/healthcheck.ts
@@ -1,8 +1,14 @@
 import type { Context, APIGatewayProxyStructuredResultV2, APIGatewayProxyEventV2, Handler } from "aws-lambda";
 
 export const handler: Handler = async (_event: APIGatewayProxyEventV2, _context: Context): Promise<APIGatewayProxyStructuredResultV2> => {
+  const list = Object.keys(process.env);
+  const envs = list.reduce((acc:any, key:any) => {
+    acc[key] = process.env[key];
+    return acc;
+  },{});
+  console.log(envs);
   return {
-    body: "OK",
+    body: JSON.stringify({ message: "Healthcheck OK" }),
     statusCode: 200,
   };
 };
