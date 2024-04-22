966e3c0dc6748896599c9ee91ad3ec283354318b
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2024-04-22 18:27:09 -0300

    teste envio de mensagem via whats

--- src/functions/hookMessage.ts
+++ src/functions/hookMessage.ts
@@ -1,12 +1,27 @@
+import { Z_APIWhatsAppAdapter } from "../infra/adapter/Z_APIWhatsAppAdapter";
+import { WhatsAppService } from "../services/WhatsAppService";
 
+const whatsaapService = new WhatsAppService(new Z_APIWhatsAppAdapter());
 export const handler = async (event: any, _context: any) => {
+  const body = JSON.parse(event.body);
 
-    const body = JSON.parse(event.body);
+  console.log(body);
 
-    console.log(`Received message: ${body}`);
-    
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Message received'}),
-    };
+  await whatsaapService
+    .sendMessage({
+      phone: "15981785706",
+      message: JSON.stringify(body),
+      title: "*TPE Digital - Message*",
+      linkUrl: `${process.env.FRONTEND_URL}`,
+      linkDescription: "teste hook",
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  console.log(`Mensagem enviada com sucesso`);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Message received" }),
+  };
 };
