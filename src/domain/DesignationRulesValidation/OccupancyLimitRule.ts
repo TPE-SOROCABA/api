cf9795da5dda652cb010e0fe7e441069f2b95ae6
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2024-05-01 22:34:49 -0300

    adicionando contador OccupancyLimitRule

--- src/domain/DesignationRulesValidation/OccupancyLimitRule.ts
+++ src/domain/DesignationRulesValidation/OccupancyLimitRule.ts
@@ -8,6 +8,7 @@ export class OccupancyLimitRule implements ValidationPlugin {
     designation.assignments.forEach((assignment) => {
       if (assignment.participants.length > assignment.config.max) {
         assignment.error = `O ponto ${assignment.point.name} não pode ter mais participantes do que o limite de ocupação`;
+        count++;
       }
     })
     if (count) throw new BadRequestException("O ponto não pode ter mais participantes do que o limite de ocupação");
