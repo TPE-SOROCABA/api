94ff2b1ec559f49458c06aa438c8e717298834d1
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2025-03-29 19:07:19 -0300

    feat(WhatsAppService): simulate message sending in development mode

--- src/services/WhatsAppService.ts
+++ src/services/WhatsAppService.ts
@@ -1,9 +1,15 @@
 import { WhatsAppAdapterSendMessage, WhatsAppAdapter } from "../interface/WhatsAppAdapter";
 
 export class WhatsAppService {
-  constructor(readonly whatsAppAdapter: WhatsAppAdapter) {}
+  constructor(readonly whatsAppAdapter: WhatsAppAdapter) { }
 
   async sendMessage(data: WhatsAppAdapterSendMessage) {
+    if (process.env.NODE_ENV === "dev") {
+      console.log(`Simulating WhatsApp message sending in dev mode`);
+      console.log(data);
+      await new Promise((resolve) => setTimeout(resolve, Math.floor(Math.random() * 300)));
+      return;
+    }
     await this.whatsAppAdapter.sendMessage(data);
   }
 }
